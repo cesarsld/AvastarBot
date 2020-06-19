5b0e66c216cf5b563b59366c0c8c9c954a5483eb
cesarsld <jaimes.cesar88@gmail.com>
2020-06-19 18:40:13 +0200

    Bugfix when 0 traits

--- AvastarBot/AvastarCommands.cs
+++ AvastarBot/AvastarCommands.cs
@@ -64,6 +64,8 @@ namespace AvastarBot
                     }
                 }
             }
+            if (returnStr.Length == 0)
+                return "None";
             return returnStr;
         }
 
